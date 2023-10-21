a35127158fc8da8f222e615d06b9226249a1dd63
Samiul Hasan <106056497+samiul-hasan21@users.noreply.github.com>
2023-10-21 10:52:12 +0600

    Add files via upload

--- A. Sum of Three.cpp
+++ A. Sum of Three.cpp
@@ -2,34 +2,29 @@
 
 using namespace std;
 
-int n;
-
-inline void read() {
-	cin >> n;
-}
-
-inline void solve() {
-	for (int x = 1; x <= 20; x++) {
-	    for (int y = 1; y <= 20; y++) {
-	        if (x + y >= n || x == y) continue;
-	        int z = n - x - y;
-	        if (z == x || z == y) continue;
-	        if (x % 3 == 0 || y % 3 == 0 || z % 3 == 0) {
-	            continue;
-	        }
-	        puts("YES");
-	        cout << x << ' ' << y << ' ' << z << endl;
-	        return;
-	    }
-	}
-	puts("NO");
-}
-
-int main () {
+int main() {
     int t;
     cin >> t;
-    while (t--){
-        read();
-        solve();
+    while (t--) {
+        int n;
+        cin >> n;
+
+        for (int x = 1; x <= 20; x++) {
+            for (int y = 1; y <= 20; y++) {
+                if (x + y >= n || x == y) continue;
+                int z = n - x - y;
+                if (z == x || z == y) continue;
+                if (x % 3 == 0 || y % 3 == 0 || z % 3 == 0) {
+                    continue;
+                }
+                puts("YES");
+                cout << x << ' ' << y << ' ' << z << endl;
+                goto end_loop;
+            }
+        }
+        puts("NO");
+
+    end_loop:
+        continue;
     }
 }
